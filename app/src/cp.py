3109f99a1e815dec0ead6614617bd3d410c14ba2
Sumeet Pareek <positivecharge@gmail.com>
2012-03-31 10:56:19 +0530

    Added all things necessary for data imports

--- app/src/cp.py
+++ app/src/cp.py
@@ -3,6 +3,8 @@ import webapp2
 import jinja2
 import os
 
+from schema import *
+
 # We initialize the templating engine with the current files path
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -12,6 +14,11 @@ class MainPage(webapp2.RequestHandler):
     # Fetch the index.html template and render it
     template = jinja_environment.get_template('templates/index.html')
     self.response.out.write(template.render())
+#    team = Team()
+#    team.put()
+#    match = Match()
+#    match.put()
+    
 
 # Once app.yaml sends us here we call use appropriate clases to show functionality for appropriate paths
 app = webapp2.WSGIApplication([
