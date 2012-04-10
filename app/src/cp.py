4688d99ade44430877849b1274024468fb5cf1dc
Sumeet Pareek <positivecharge@gmail.com>
2012-04-10 07:07:44 +0530

    facebook share with perma url and human pred messages added

--- app/src/cp.py
+++ app/src/cp.py
@@ -90,7 +90,7 @@ class MainPage(BaseHandler):
     ''' 
 #    current_datetime = datetime.datetime.now() #TODO real val to use
 #    current_datetime = datetime.datetime.strptime('Fri Apr 6 2012 18:00','%a %b %d %Y %H:%M') #TODO temp val to use
-    current_datetime = datetime.datetime.strptime('Fri Apr 6 2012 18:00','%a %b %d %Y %H:%M') #TODO temp val to use
+    current_datetime = datetime.datetime.strptime('Fri Apr 20 2012 18:00','%a %b %d %Y %H:%M') #TODO temp val to use
     for match_key in cp_data['match_keys']:
       match_datetime = cp_data['matches'][match_key]['start_time']
       match_time_delta = match_datetime - current_datetime
@@ -98,13 +98,18 @@ class MainPage(BaseHandler):
         cp_data['matches'][match_key]['flag'] = 'future'
         cp_data['matches'][match_key]['pred_start_time'] = (match_datetime - datetime.timedelta(days=1))
       elif (match_time_delta.days < 0):
-        cp_data['matches'][match_key]['flag'] = 'closed' #TODO: add pending check later
+        # If scores of this match are updated then the match is 'closed' else it is 'pending'
+        if cp_data['matches'][match_key]['key'] in match_team_stats :
+          cp_data['matches'][match_key]['flag'] = 'closed' 
+        else:
+          cp_data['matches'][match_key]['flag'] = 'pending'
       else:
         cp_data['matches'][match_key]['flag'] = 'open'
     
     template_values = {
       'cp': cp_data,
       'current_user': self.current_user,
+      'current_user_key': str(self.current_user.key()),
       'request': self.request,
       'environ': os.environ,
       'facebook_app_id': FACEBOOK_APP_ID,
--- app/src/static/js/script.js
+++ app/src/static/js/script.js
@@ -1,3 +1,25 @@
+function postToFeed(url, name_val, caption_val, desc_val) {
+	var obj = {
+	  method: 'feed',
+	  link: url,
+	//  picture: 'http://localhost:8080/static/css/images/logo.png', //TODO use  each match specific image here
+	  name: name_val,
+	  caption: 'Think you can better this!?',
+	  description: desc_val
+	};
+	
+	function callback(response) {
+		if (response != null) {
+			show_action_message("Done! That was cool :-)");
+		} else {
+			show_action_message("Your predictions were not shared :-(");
+		}
+	}
+	
+	FB.ui(obj, callback);
+}
+
+
 // Function to show "action message" and then hide it
 function show_action_message(msg) {
 	var p = $('.content').position();
@@ -29,7 +51,72 @@ $.fn.serializeObject = function()
 };
 
 // Things to do when the document is all ready
-$(document).ready(function(){
+$(document).ready(function() {
+	// On click of .fb-share-btn's > we find parent .match-main > using the form values create a message to post
+	$('.fb-share-btn').click(function(){
+		// get the concerned match
+		var parent_match_main = $(this).parents('.match-main');
+		
+		// form the message title
+		var teams = parent_match_main.find('h1').text();
+		var title = 'My predictions for the ' + teams + ' game!';
+		
+		// Grab predictions permanent URL
+		var perma_url = parent_match_main.find('input[name="perma_url"]').val();
+		
+		// form the message description to be shared
+		var player_run = parent_match_main.find('select.match_player[name="player_run"] option:selected').text(); 
+		var player_wicket = parent_match_main.find('select.match_player[name="player_wicket"] option:selected').text();
+		var player_six = parent_match_main.find('select.match_player[name="player_six"] option:selected').text();
+		var team_run = parent_match_main.find('select.match_player[name="team_run"] option:selected').text();
+		var team_six = parent_match_main.find('select.match_player[name="team_six"] option:selected').text(); 
+		var pred_player_run = parent_match_main.find('input[name="player_run_pred"]').val();
+		var pred_player_wicket = parent_match_main.find('input[name="player_wicket_pred"]').val();
+		var pred_player_six = parent_match_main.find('input[name="player_six_pred"]').val();
+		var pred_team_run = parent_match_main.find('input[name="team_run_pred"]').val();
+		var pred_team_six = parent_match_main.find('input[name="team_six_pred"]').val();
+		
+		
+		var message = '';
+		if (pred_player_run == 1) {
+			message += 'I predict - \n (1) ' + player_run + ' will score just ' + pred_player_run + ' run (2) ';
+		} else {
+			message += 'I predict - \n (1) ' + player_run + ' will score about ' + pred_player_run + ' runs (2) ';
+		}
+		if (pred_player_wicket == 1) {
+			message += player_wicket + ' will take just ' + pred_player_wicket + ' wicket (3) ';
+		} else {
+			message += player_wicket + ' will take ' + pred_player_wicket + ' wickets (3) ';
+		}
+		if (pred_player_six == 1) {
+			message += player_six + ' will hit exactly ' + pred_player_six + ' six (4) ';
+		} else {
+			message += player_six + ' will hit ' + pred_player_six + ' sixes (4) ';
+		}
+		if (pred_team_run == 1) {
+			message += team_run + ' will score just ' + pred_team_run + ' run .. and .. (5) ';
+		} else {
+			message += team_run + ' will score about ' + pred_team_run + ' runs .. and .. (5) ';
+		}
+		if (pred_team_six == 1) {
+			message += team_six + ' will hit exactly' + pred_team_six + ' six !!';
+		} else {
+			message += team_six + ' will hit ' + pred_team_six + ' sixes !!';
+		}
+		
+		postToFeed(perma_url, title, teams, message)
+		return false;
+	});
+	
+	
+	// If there is schedule in the sidebar, scroll to keep the open match at the center
+	var container = $('div.schedule-content');
+	var scrollTo = $('div.match-link-open:first');
+	container.scrollTop(
+	    scrollTo.offset().top - container.offset().top + container.scrollTop() - 140
+	);
+	
+	
 	// Show/Hide the rules when hovering on the rules link
 	$("#rules-link").mouseenter(function() {
 		var p = $(this).position();
@@ -49,10 +136,6 @@ $(document).ready(function(){
 		});			
 	});
 	
-//	$('body.anon-user .match-main form').submit(function(){
-//		alert("Please login to submit your info. (TODO: Take to the login popup and auto submit)");
-//		return false;
-//	});
 	
 	// Submit predictions via AJAX and show the status
 	$('body.auth-user .match-main form').submit(function(){
@@ -67,6 +150,9 @@ $(document).ready(function(){
 					$('.match-main-open.active form input.submit').attr('disabled',false).attr('value','Update Your Predictions');
 					$('.match-main-open.active form input[name=pred_key]').attr('value', response.pred_key);
 					show_action_message('Done! You can now share your predictions with friends.');
+					$('.match-main-open.active .make-match-pred-msg').slideUp('slow', function(){
+						$('.match-main-open.active .share-match-pred-msg').slideDown('slow');
+					});
 				}
 				else {
 					$('.match-main-open.active form input.submit').attr('disabled',false).attr('value','Save Your Predictions');
